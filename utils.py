f71a4d3af5186507ab2e7f0b0eb4f36dea036c7d
Yanling Liu <30328162+yliu7366@users.noreply.github.com>
2024-01-10 16:10:04 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,17 @@
 # utility helpers
+def byteLiteralList2String(bl):
+  """
+  convert a list of byte literals to string
+  for example: bl: b'0' b'.' b'0' b'0' b'5' b'2'
+               bl_str: "0.0052"
+  Imaris/HDF save strings as lists of byte literals
+  :param bl: a list of byte literals
+  :return: a string version of the input binary literal list
+  """
+  bl_list = []
+  for c in bl:
+    bl_list.append(c.decode())
+  str = ""
+  bl_str = str.join(bl_list)
+
+  return bl_str
